807d9cf1bd068930bb4b83644f8a2c0b0fe7cd71
Nicolas Meseth <nicolas.meseth@googlemail.com>
2022-02-28 17:02:48 +0000

    Added math script

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# programming-with-r
-Material for the introduction to programming with R.
+# Programming with R
+
+This repository contains the material for the introduction to programming with R. It was created by Nicolas Meseth at the [University of Applied Sciences Osnabrück](https://www.hs-osnabrueck.de/)
---
+++ scripts/01_math_with_r.R
@@ -0,0 +1,72 @@
+# You can use R and the R console as an advanced calculator.
+# The following script demonstrates how to apply some mathematical operations
+# with R.
+
+# When using RStudio, place the cursor to a line you want to execute and press 
+# "Run" or hit Ctrl + Enter. This will execute the command at the cursor and 
+# print the result to the R console below.
+
+# 1 Simple arithmetic ----
+
+# 1.1 Addition
+1 + 1
+
+# 1.2 Subtraction
+2 - 1
+
+# 1.3 Division
+256 / 16
+
+# 1.4 Multiplication
+16 * 16 * 16
+
+# 1.5 Modulo
+11 %% 2
+
+# 2 Logic ----
+
+# 2.1 Equals
+1 == 1
+1 == 2
+
+# 2.2 Less/greater
+1 < 2
+1 > 2
+
+2 >= 2
+2 <= 2
+
+# 3 Roots and power ----
+
+# 3.1 Square root
+sqrt(16)
+
+# 3.2 Other roots
+64^(1/3)
+
+# 3.3 Power
+16^2
+
+# 4 Logarithms and exponential ----
+
+# 4.1 Natural logarithm
+log(10)
+
+# 4.2 Other logarithms
+log(64, 2)
+
+# 5 Rounding numbers ----
+
+# 5.1 
+round(1/3, 3)
+round(pi, 4)
+
+# 6 Other math functions ----
+
+# 6.1 Geometry
+sin(1)
+cos(1)
+tan(1)
+
+# 6.2 Absolute numbers
+abs(-2)
